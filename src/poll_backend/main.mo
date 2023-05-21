df884ed23ea6a7c3a1c1cb6d523c9d3c042eb5fb
Nguyen Viet Tien <viettiennguyen029@gmail.com>
2023-05-21 18:14:00 +0700

    add project files

--- src/poll_backend/main.mo
+++ src/poll_backend/main.mo
@@ -1,5 +1,45 @@
+import Iter "mo:base/Iter";
+import Nat "mo:base/Nat";
+import RBTree "mo:base/RBTree";
+import Text "mo:base/Text";
+
 actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+  var question : Text = "What is your favorite programing laguage ?";
+  var votes : RBTree.RBTree<Text, Nat> = RBTree.RBTree(Text.compare);
+
+  // query call mean that it does not change any data
+  public query func getQuestion() : async Text {
+    question;
   };
+
+  public query func getVotes() : async [(Text, Nat)] {
+    Iter.toArray(votes.entries());
+  };
+
+  public func vote(entry : Text) : async [(Text, Nat)] {
+    // Check if entry is voted or not
+    let votes_for_entry : ?Nat = votes.get(entry);
+
+    let current_vote_for_entry : Nat = switch votes_for_entry {
+      case null 0;
+      case (?Nat) Nat;
+    };
+
+    // once we have the current number of votes, update new value
+    votes.put(entry, current_vote_for_entry + 1);
+
+    // return
+    Iter.toArray(votes.entries());
+  };
+
+  public func resetVotes() : async [(Text, Nat)] {
+    votes.put("Motoko", 0);
+    votes.put("Rust", 0);
+    votes.put("Type Script", 0);
+    votes.put("Python", 0);
+
+    // return
+    Iter.toArray(votes.entries());
+  };
+
 };
--- src/poll_frontend/src/index.js
+++ src/poll_frontend/src/index.js
@@ -1,19 +1,77 @@
+const pollForm = document.getElementById("radioForm");
+const resultsDiv = document.getElementById('results');
+const resetButton = document.getElementById('reset');
+
 import { poll_backend } from "../../declarations/poll_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
+
+//1. LOCAL DATA
+const pollResults = {
+  "Rust": 0,
+  "Motoko": 0,
+  "TypeScript": 0,
+  "Python": 0
+};
+
+//2. EVENT LISTENERS
+//Load the Simple Poll's question from the backend when the app loads
+document.addEventListener('DOMContentLoaded', async (e) => {
   e.preventDefault();
-  const button = e.target.querySelector("button");
 
-  const name = document.getElementById("name").value.toString();
+  // Query the question from the backend
+  const question = await poll_backend.getQuestion();
+  document.getElementById("question").innerText = question;
 
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await poll_backend.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
+  const voteCounts = await poll_backend.getVotes();
+  updateLocalVoteCounts(voteCounts);
+  displayResults();
   return false;
-});
+}, false);
+
+pollForm.addEventListener('submit', async (e) => {
+  //note that this is at beginning of the submit callback, this is deliberate
+  //This is so the default behavior is set BEFORE the awaits are called below
+  e.preventDefault(); 
+
+  const formData = new FormData(pollForm);
+  const checkedValue = formData.get("option");
+
+  const updatedVoteCounts = await poll_backend.vote(checkedValue);
+  console.log("Returning from await...")
+  console.log(updatedVoteCounts);
+  updateLocalVoteCounts(updatedVoteCounts);
+  displayResults();
+  return false;
+}, false);
+
+resetButton.addEventListener('click', async (e) => {
+  e.preventDefault();
+  
+  //Reset the options in the backend
+  await poll_backend.resetVotes();
+  const voteCounts = await poll_backend.getVotes();
+  updateLocalVoteCounts(voteCounts);
+
+  //re-render the results once the votes are reset in the backend
+  displayResults();
+  return false;
+}, false);
+
+//3. HELPER FUNCTIONS
+function displayResults() {
+  let resultHTML = '<ul>';
+  for (let key in pollResults) {
+      resultHTML += '<li><strong>' + key + '</strong>: ' + pollResults[key] + '</li>';
+  }
+  resultHTML += '</ul>';
+  resultsDiv.innerHTML = resultHTML;
+};
+
+function updateLocalVoteCounts(arrayOfVoteArrays){
+  for (let voteArray of arrayOfVoteArrays) {
+    //Example voteArray -> ["Motoko","0"]
+    let voteOption = voteArray[0];
+    let voteCount = voteArray[1];
+    pollResults[voteOption] = voteCount;
+  }
+};
